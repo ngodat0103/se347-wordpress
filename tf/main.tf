e7fb7c6f9d18ca813564edc894ef990e6152977f
ngodat0103 <ngovuminhdat@gmail.com>
2024-12-05 19:09:45 +0700

    update

--- tf/main.tf
+++ tf/main.tf
@@ -25,14 +25,11 @@ module "instances" {
     docker volume create wordpress-db
     tar -xvf mysql_backup.tar --strip 3 -C /var/lib/docker/volumes/wordpress-db/_data/
     tar -xvf wordpress_backup.tar --strip 3 -C /var/lib/docker/volumes/wordpress/_data/
-    cd /opt
-    # git clone https://github.com/ngodat0103/se347-wordpress.git
-    # Create a .env file manually
-    # and using docker compose up -d to start the wordpress
     cd /home/ubuntu
-    chown -R ubuntu:ubuntu /opt/se347-wordpress
-    ln -sf /opt/se347-wordpress/ se347-wordpress
-    chown -R ubuntu:ubuntu /home/ubuntu/se347-wordpress
+    git clone https://github.com/ngodat0103/se347-wordpress.git
+    chown ubuntu:ubuntu se347-wordpress
+    #Create a .env file manually
+    #and using docker compose up -d to start the wordpress
     EOF
 }
 module "firewall" {
