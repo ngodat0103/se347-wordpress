f3293572704135588431f290c8f8557679e5489d
ngodat0103 <ngovuminhdat@gmail.com>
2024-12-05 17:09:16 +0700

    update

--- tf/main.tf
+++ tf/main.tf
@@ -7,26 +7,28 @@ module "instances" {
     machine_type = "e2-medium"
     source = "./modules/instances"
     target_tags=["http","https"]
+    pub_key_paths = ["~/.ssh/id_rsa.pub","/home/akira/code/se347-wordpress/minh-key.pub"]
+    environment = "se347-demo"
     startup_script = <<EOF
-        #!/bin/bash
-        mkdir -p /opt/init-script 
-        cd /opt/init-script
-        git clone https://github.com/ngodat0103/common-stuff.git
-        cd common-stuff/0-shell/0-Installation
-        chmod +x docker-installation.sh
-        ./docker-installation.sh
-        usermod -aG docker ubuntu
-        wget http://nextcloud.ngodat0103.live/index.php/s/wordpress/download/iAViXXr.7z
-        apt install -y p7zip-full -y
-        7z x iAViXXr.7z 
-        docker volume create wordpress
-        docker volume create wordpress-data
-        tar -xvf mysql_backup.tar --strip 3 -C /var/lib/docker/volumes/wordpress-db/_data/
-        tar -xvf wordpress_backup.tar --strip 3 -C /var/lib/docker/volumes/wordpress/_data/
-        cd /opt
-        git clone https://github.com/ngodat0103/se347-wordpress.git
-        # Create a .env file manually
-        # and using docker compose up -d to start the wordpress
+    #!/bin/bash
+    mkdir -p /opt/init-script 
+    cd /opt/init-script
+    git clone https://github.com/ngodat0103/common-stuff.git
+    cd common-stuff/0-shell/0-Installation
+    chmod +x docker-installation.sh
+    ./docker-installation.sh
+    usermod -aG docker ubuntu
+    wget http://nextcloud.ngodat0103.live/index.php/s/wordpress/download/iAViXXr.7z
+    apt install -y p7zip-full -y
+    7z x iAViXXr.7z 
+    docker volume create wordpress
+    docker volume create wordpress-data
+    tar -xvf mysql_backup.tar --strip 3 -C /var/lib/docker/volumes/wordpress-db/_data/
+    tar -xvf wordpress_backup.tar --strip 3 -C /var/lib/docker/volumes/wordpress/_data/
+    cd /opt
+    # git clone https://github.com/ngodat0103/se347-wordpress.git
+    # Create a .env file manually
+    # and using docker compose up -d to start the wordpress
     EOF
 }
 module "firewall" {
